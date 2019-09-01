aa96a74aabf20284999907e4be5f41827303bd31
Martin Doucha <next_ghost@quick.cz>
2019-09-01 22:11:22 +0200

    Add license

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+/*
+Wiki-crosslang - App for looking up bilingual texts on Wikisource
+Copyright (C) 2019 next_ghost
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 var slow_url = null;
 
 function add_init_callback(callback) {
